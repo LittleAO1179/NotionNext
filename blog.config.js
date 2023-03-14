8c37ff48dfb2bafc5abe41b4f2bff0b85d4511c3
LittleAO1179 <121712114+LittleAO1179@users.noreply.github.com>
2023-03-14 21:53:16 +0800

    update

--- blog.config.js
+++ blog.config.js
@@ -8,7 +8,7 @@ const BLOG = {
     THEME_SWITCH: process.env.NEXT_PUBLIC_THEME_SWITCH || false, // 是否显示切换主题按钮
     LANG: process.env.NEXT_PUBLIC_LANG || 'zh-CN', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
     SINCE: 2021, // e.g if leave this empty, current year will be used.
-    APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
+    APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'dark', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
 
     CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
@@ -83,7 +83,7 @@ const BLOG = {
     PREVIEW_TAG_COUNT: 16, // 首页最多展示的标签数量，0为不限制
 
     // 鼠标点击烟花特效
-    FIREWORKS: process.env.NEXT_PUBLIC_FIREWORKS || false, // 开关
+    FIREWORKS: process.env.NEXT_PUBLIC_FIREWORKS || true, // 开关
     // 烟花色彩，感谢 https://github.com/Vixcity 提交的色彩
     FIREWORKS_COLOR: ['255, 20, 97', '24, 255, 146', '90, 135, 255', '251, 243, 140'],
 
@@ -102,7 +102,7 @@ const BLOG = {
     STARRY_SKY: process.env.NEXT_PUBLIC_STARRY_SKY || false, // 开关
 
     // 悬浮挂件
-    WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || true, // 是否显示宠物挂件
+    WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || false, // 是否显示宠物挂件
     WIDGET_PET_LINK: process.env.NEXT_PUBLIC_WIDGET_PET_LINK || 'https://cdn.jsdelivr.net/npm/live2d-widget-model-wanko@1.0.5/assets/wanko.model.json', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
     WIDGET_PET_SWITCH_THEME: true, // 点击宠物挂件切换博客主题
 
