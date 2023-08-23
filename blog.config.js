d24d71981406319bfe305812fb84fda746c54ef1
LittleAO <1179337245@qq.com>
2023-08-23 22:18:56 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
     AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'LittleAO', // 您的昵称 例如 tangly1024
     BIO: process.env.NEXT_PUBLIC_BIO || '还在学习🥱', // 作者简介
     LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.littleao.top', // 网站地址
-    KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '学习, 博客', // 网站关键词 英文逗号隔开
+    KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '学习, 博客，计算机', // 网站关键词 英文逗号隔开
 
     // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
     CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'limingyang1179@163.com', // 邮箱地址 例如mail@tangly1024.com
